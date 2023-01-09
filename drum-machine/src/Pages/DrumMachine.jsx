7b07e7a19d7df6fe098db032699338b05395bede
Adrian Nads <marcelonads@gmail.com>
2023-01-09 10:35:49 +0800

    DONE for the Drum Machine
    FIXED the error of setAudioClip

--- drum-machine/src/Pages/DrumMachine.jsx
+++ drum-machine/src/Pages/DrumMachine.jsx
@@ -12,46 +12,88 @@ class DrumMachine extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            
+            audioClip: ''
+        }
+
+        this.setAudioClip = this.setAudioClip.bind(this);
     }
 
     componentDidMount() {
 
         document.title = 'Drum Machine | adriandotdev';
-
+        
         document.addEventListener('keypress', (e) => {
 
             if (e.key.toLowerCase() === 'q') {
                 console.log("Q is pressed.");
 
-                document.getElementById('Q-audio').play();
+                document.getElementById('Q').play();
+
+                this.setState({audioClip : 'Heater 1'});
             }
             else if (e.key.toLowerCase() === 'w') {
                 console.log("W is pressed.");
+
+                document.getElementById('W').play();
+
+                this.setState({audioClip : 'Heater 2'});
             }
             else if (e.key.toLowerCase() === 'e') {
                 console.log("E is pressed.");
+
+                document.getElementById('E').play();
+
+                this.setState({audioClip : 'Heater 3'});
             }
             else if (e.key.toLowerCase() === 'a') {
                 console.log("A is pressed.");
+                document.getElementById('A').play();
+
+                this.setState({audioClip : 'Heater 4'});
             }
             else if (e.key.toLowerCase() === 's') {
                 console.log("S is pressed.");
+                document.getElementById('S').play();
+
+                this.setState({audioClip : 'Clap'});
             }
             else if (e.key.toLowerCase() === 'd') {
                 console.log("D is pressed.");
+                document.getElementById('D').play();
+
+                this.setState({audioClip : 'Open-HH'});
             }
             else if (e.key.toLowerCase() === 'z') {
                 console.log("Z is pressed.");
+                document.getElementById('Z').play();
+
+                this.setState({audioClip : 'Kick and Hat'});
             }
             else if (e.key.toLowerCase() === 'x') {
                 console.log("X is pressed.");
+                document.getElementById('X').play();
+
+                this.setState({audioClip : 'Kick'});
             }
             else if (e.key.toLowerCase() === 'c') {
                 console.log("C is pressed.");
+                document.getElementById('C').play();
+
+                this.setState({audioClip : 'Closed-HH'});
+            }
+            else {
+                this.setState({audioClip : ''});
             }
         })
     }
 
+    setAudioClip(audioClip) {
+
+        this.setState({audioClip: audioClip});
+    }
     render() {
         return (
             <>
@@ -62,34 +104,34 @@ class DrumMachine extends React.Component {
                         <Row style={{minHeight: '100vh'}} className="row-cols-1 justify-content-center">
                             <Col className="d-flex flex-column justify-content-center align-items-center">
                                 <h1 id="main-title" className="display-1 text-center fw-bold">Drum Machine</h1>
-                                <p id="display" className="display-3 mb-4">Result</p>
+                                <p id="display" className="display-3 mb-4">{this.state.audioClip}</p>
                                   <Row className="row row-cols-3 row-cols-xxl-12 gap-4 justify-content-center">
                                     <Col className="w-auto">
-                                        <DrumPad text="Q"/>
+                                        <DrumPad text="Q" audioFile="Heater-1.mp3" audioFileName="Heater 1" setAudioClip={this.setAudioClip}/>
+                                    </Col>
+                                    <Col className="w-auto" >
+                                        <DrumPad text="W" audioFile="Heater-2.mp3" audioFileName="Heater 2" setAudioClip={this.setAudioClip}/>
                                     </Col>
                                     <Col className="w-auto">
-                                        <DrumPad text="W"/>
+                                        <DrumPad text="E" audioFile="Heater-3.mp3" audioFileName="Heater 3" setAudioClip={this.setAudioClip}/>
                                     </Col>
                                     <Col className="w-auto">
-                                        <DrumPad text="E"/>
+                                        <DrumPad text="A" audioFile="Heater-4.mp3" audioFileName="Heater 4" setAudioClip={this.setAudioClip}/>
                                     </Col>
                                     <Col className="w-auto">
-                                        <DrumPad text="A"/>
+                                        <DrumPad text="S" audioFile="Clap.mp3" audioFileName="Clap" setAudioClip={this.setAudioClip}/>
                                     </Col>
                                     <Col className="w-auto">
-                                        <DrumPad text="S"/>
+                                        <DrumPad text="D" audioFile="Open-HH.mp3" audioFileName="Open HH" setAudioClip={this.setAudioClip}/>
                                     </Col>
                                     <Col className="w-auto">
-                                        <DrumPad text="D"/>
+                                        <DrumPad text="Z" audioFile="Kick_n_Hat.mp3" audioFileName="Kick and Hat" setAudioClip={this.setAudioClip}/>
                                     </Col>
                                     <Col className="w-auto">
-                                        <DrumPad text="Z"/>
+                                        <DrumPad text="X" audioFile="Kick.mp3" audioFileName="Kick" setAudioClip={this.setAudioClip}/>
                                     </Col>
                                     <Col className="w-auto">
-                                        <DrumPad text="X"/>
-                                    </Col>
-                                    <Col className="w-auto">
-                                        <DrumPad text="C"/>
+                                        <DrumPad text="C" audioFile="Closed-HH.mp3" audioFileName="Closed-HH" setAudioClip={this.setAudioClip}/>
                                     </Col>
                                 </Row>
                             </Col>
--- drum-machine/src/components/DrumPad.jsx
+++ drum-machine/src/components/DrumPad.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-constructor */
 import React from 'react'
 // import '../../public/audio/Heater-1.mp3'
 class DrumPad extends React.Component {
@@ -11,9 +12,13 @@ class DrumPad extends React.Component {
     const PATH = '/audio/';
 
     return (
-      <div id={this.props.text} style={{width: '4rem', height: '5rem', padding: '3rem'}} className="drum-pad d-flex justify-content-center align-items-center bg-dark">
+      <div onClick={() => {
+        document.getElementById(this.props.text).play();
+
+        this.props.setAudioClip(this.props.audioFileName);
+      }} id={this.props.text + " pad"} style={{width: '4rem', height: '5rem', padding: '3rem'}} className="drum-pad d-flex justify-content-center align-items-center bg-dark">
         <span className='display-5 fw-bold'>{this.props.text}</span>
-        <audio id={this.props.text + "-audio"} autoplay>
+        <audio src={PATH + this.props.audioFile} class="clip" id={this.props.text}>
             <source src={PATH + this.props.audioFile} type="audio/mpeg" ></source>
             Hello
         </audio>
