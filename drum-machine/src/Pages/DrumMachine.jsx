9bd9c3cc86543b0b18b25d6052b57006022bc2f6
Adrian Nads <marcelonads@gmail.com>
2023-01-09 08:11:37 +0800

    SETUP the audio files needed

--- drum-machine/src/Pages/DrumMachine.jsx
+++ drum-machine/src/Pages/DrumMachine.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import {Container, Row, Col} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../App.css'
+import '../App.css';
+
+
+// COMPONENTS
+import DrumPad from '../components/DrumPad';
+
 class DrumMachine extends React.Component {
 
     constructor(props) {
@@ -12,44 +17,88 @@ class DrumMachine extends React.Component {
     componentDidMount() {
 
         document.title = 'Drum Machine | adriandotdev';
+
+        document.addEventListener('keypress', (e) => {
+
+            if (e.key.toLowerCase() === 'q') {
+                console.log("Q is pressed.");
+
+                document.getElementById('Q-audio').play();
+            }
+            else if (e.key.toLowerCase() === 'w') {
+                console.log("W is pressed.");
+            }
+            else if (e.key.toLowerCase() === 'e') {
+                console.log("E is pressed.");
+            }
+            else if (e.key.toLowerCase() === 'a') {
+                console.log("A is pressed.");
+            }
+            else if (e.key.toLowerCase() === 's') {
+                console.log("S is pressed.");
+            }
+            else if (e.key.toLowerCase() === 'd') {
+                console.log("D is pressed.");
+            }
+            else if (e.key.toLowerCase() === 'z') {
+                console.log("Z is pressed.");
+            }
+            else if (e.key.toLowerCase() === 'x') {
+                console.log("X is pressed.");
+            }
+            else if (e.key.toLowerCase() === 'c') {
+                console.log("C is pressed.");
+            }
+        })
     }
 
     render() {
         return (
             <>
-                <Container id="drum-machine">
-                    <Row>
-                        <Col className="d-flex gap-2">
-                            
-                            <div className="drum-pad">
-                                W
-                            </div>
-                            <div className="drum-pad">
-                                E
-                            </div>
-                            <div className="drum-pad">
-                                A
-                            </div>
-                            <div className="drum-pad">
-                                S
-                            </div>
-                            <div className="drum-pad">
-                                D
-                            </div>
-                            <div className="drum-pad">
-                                Z
-                            </div>
-                            <div className="drum-pad">
-                                X
-                            </div>
-                            <div className="drum-pad">
-                                C
-                            </div>
-                        </Col>
-                        <Col>
-                        </Col>
-                    </Row>
-                </Container>
+                <div className="bg-image">
+                    
+                    <Container style={{minHeight: '100vh'}} id="drum-machine" className="">
+                        
+                        <Row style={{minHeight: '100vh'}} className="row-cols-1 justify-content-center">
+                            <Col className="d-flex flex-column justify-content-center align-items-center">
+                                <h1 id="main-title" className="display-1 text-center fw-bold">Drum Machine</h1>
+                                <p id="display" className="display-3 mb-4">Result</p>
+                                  <Row className="row row-cols-3 row-cols-xxl-12 gap-4 justify-content-center">
+                                    <Col className="w-auto">
+                                        <DrumPad text="Q"/>
+                                    </Col>
+                                    <Col className="w-auto">
+                                        <DrumPad text="W"/>
+                                    </Col>
+                                    <Col className="w-auto">
+                                        <DrumPad text="E"/>
+                                    </Col>
+                                    <Col className="w-auto">
+                                        <DrumPad text="A"/>
+                                    </Col>
+                                    <Col className="w-auto">
+                                        <DrumPad text="S"/>
+                                    </Col>
+                                    <Col className="w-auto">
+                                        <DrumPad text="D"/>
+                                    </Col>
+                                    <Col className="w-auto">
+                                        <DrumPad text="Z"/>
+                                    </Col>
+                                    <Col className="w-auto">
+                                        <DrumPad text="X"/>
+                                    </Col>
+                                    <Col className="w-auto">
+                                        <DrumPad text="C"/>
+                                    </Col>
+                                </Row>
+                            </Col>
+                            {/* <Col className="bg-info align-self-start">
+                              
+                            </Col> */}
+                        </Row>
+                    </Container>
+                </div>
             </>
         );
     }
--- drum-machine/src/components/DrumPad.jsx
+++ drum-machine/src/components/DrumPad.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+// import '../../public/audio/Heater-1.mp3'
 class DrumPad extends React.Component {
 
   constructor(props) {
@@ -7,9 +7,16 @@ class DrumPad extends React.Component {
   }
 
   render() {
+
+    const PATH = '/audio/';
+
     return (
-      <div className="drum-pad d-flex justify-content-center align-items-center bg-dark text-white">
-        <span>{this.props.text}</span>
+      <div id={this.props.text} style={{width: '4rem', height: '5rem', padding: '3rem'}} className="drum-pad d-flex justify-content-center align-items-center bg-dark">
+        <span className='display-5 fw-bold'>{this.props.text}</span>
+        <audio id={this.props.text + "-audio"} autoplay>
+            <source src={PATH + this.props.audioFile} type="audio/mpeg" ></source>
+            Hello
+        </audio>
     </div>
     )
   }
